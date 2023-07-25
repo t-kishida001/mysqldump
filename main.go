53d05485257e7efc89b7fcb6473e0b606a6a9d2b
t0ge-kun <kasipan12345678@gmail.com>
2023-07-25 19:23:23 +0900

    fix: modify comment.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -22,22 +23,65 @@ type Config struct {
         DBPassword      string
         Databases       []string
         DumpGenerations int
-        DumpDir         string // ダンプファイルを保存するディレクトリを追加
-        DBPort          string // データベースのポート番号を追加
+        DumpDir         string
+        DBPort          string
 }
 
+// SubFunc setupLogger: Create Log dir
+func createLogDir() (string, error) {
+	logDir := filepath.Join(".", "logs")
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		if err := os.Mkdir(logDir, 0755); err != nil {
+			return "", err
+		}
+	}
+	return logDir, nil
+}
+
+// SubFunc setupLogger: Open log file "mysqldump.log"
+func openLogFile(logDir string) (*os.File, error) {
+	logFilePath := filepath.Join(logDir, "mysqldump.log")
+	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return logFile, nil
+}
+
+// MainFunc: Setup Log file
+func setupLogger() (*os.File, error) {
+	logDir, err := createLogDir()
+	if err != nil {
+		return nil, fmt.Errorf("ログフォルダの作成に失敗しました: %s", err)
+	}
+
+	logFile, err := openLogFile(logDir)
+	if err != nil {
+		return nil, fmt.Errorf("ログファイルのオープンに失敗しました: %s", err)
+	}
+
+	// set up logging to both file and standard output.
+	log.SetOutput(io.MultiWriter(logFile, os.Stdout))
+
+	return logFile, nil
+}
+
+// MainFunc: Checking for errors in the execution results
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// MainFunc: Reading config files.
 func readConfig() (*Config, error) {
-        content, err := ioutil.ReadFile(".env.txt") // envファイルのパス
+	// reading .env.txt
+        content, err := ioutil.ReadFile(".env.txt") 
         if err != nil {
                 return nil, err
         }
 
+	// setting to config get .env.txt
         lines := strings.Split(string(content), "\n")
         config := &Config{}
         for _, line := range lines {
@@ -58,17 +102,17 @@ func readConfig() (*Config, error) {
                         }
                         config.DumpGenerations = gen
                 case "DUMP_DIR":
-                        config.DumpDir = value // ダンプファイルを保存するディレクトリの指定を追加
+                        config.DumpDir = value
                 }
         }
 
-        // .sql.cnfファイルの内容から接続情報を取得
+        // reading .sql.cnf
         sqlCnfContent, err := ioutil.ReadFile(".sql.cnf")
         if err != nil {
                 return nil, err
         }
 
-        // .sql.cnfファイルの内容から接続情報を取得してconfigに設定
+	// setting to config get .sql.cnf
         lines = strings.Split(string(sqlCnfContent), "\n")
         for _, line := range lines {
                 parts := strings.SplitN(line, "=", 2)
@@ -90,7 +134,7 @@ func readConfig() (*Config, error) {
                 }
         }
 
-        // ディレクトリが指定されていない場合は、実行ディレクトリを使用する
+        // ff no directory is specified,current working directory will be used.
         if config.DumpDir == "" {
                 wd, err := os.Getwd()
                 if err != nil {
@@ -102,10 +146,12 @@ func readConfig() (*Config, error) {
         return config, nil
 }
 
+// Subfunc runMySQLActiveCheck : Create MySQL DSN(Data Source Name)
 func createMySQLDSN(config *Config) string {
         return fmt.Sprintf("%s:%s@tcp(%s:%s)/", config.DBUsername, config.DBPassword, config.DBAddress, config.DBPort)
 }
 
+// Mainfunc: Check if it can connect to MySQL.
 func runMySQLActiveCheck(config *Config) error {
         dsn := createMySQLDSN(config) // DSNを生成
         db, err := sql.Open("mysql", dsn)
@@ -122,6 +168,7 @@ func runMySQLActiveCheck(config *Config) error {
         return nil
 }
 
+// Mainfunc: Running mysqldump
 func runMySQLDump(config *Config) error {
 	for _, dbName := range config.Databases {
 		dumpCmd := exec.Command(
@@ -133,8 +180,8 @@ func runMySQLDump(config *Config) error {
 			"--set-gtid-purged=OFF",
 		)
 
-		currentTime := time.Now().Format("2006-01-02_1504") // "2006-01-02_1504"はGoの日付フォーマット
-
+		// output dump file
+		currentTime := time.Now().Format("2006-01-02_1504")
 		dumpFileName := filepath.Join(config.DumpDir, fmt.Sprintf("dump_%s_%s.sql", dbName, currentTime))
 		dumpFile, err := os.Create(dumpFileName)
 		if err != nil {
@@ -144,7 +191,7 @@ func runMySQLDump(config *Config) error {
 		defer dumpFile.Close()
 		dumpCmd.Stdout = dumpFile
 
-		// プログラムが実行するmysqldumpコマンドを標準出力に表示
+		// DEBUG: Print the mysqldump command executed by the program to standard output.
 		//fmt.Println("mysqldumpコマンド:", dumpCmd.String())
 
 		err = dumpCmd.Run()
@@ -156,6 +203,14 @@ func runMySQLDump(config *Config) error {
 	return nil
 }
 
+// Subfunc cleanupOldDumps: Time sort dump files 
+func sortByModTime(files []os.FileInfo) {
+        sort.Slice(files, func(i, j int) bool {
+                return files[i].ModTime().Before(files[j].ModTime())
+        })
+}
+
+// Mainfunc: Generation management of dump files
 func cleanupOldDumps(config *Config) error {
         files, err := ioutil.ReadDir(config.DumpDir)
         if err != nil {
@@ -167,20 +222,20 @@ func cleanupOldDumps(config *Config) error {
         for _, file := range files {
                 if strings.HasPrefix(file.Name(), "dump_") && strings.HasSuffix(file.Name(), ".sql") {
                         dbName := strings.TrimSuffix(strings.TrimPrefix(file.Name(), "dump_"), ".sql")
-                        dbName = strings.Split(dbName, "_")[0] // タイムスタンプ以降の部分を取り除く
+                        dbName = strings.Split(dbName, "_")[0] // Remove the part after timestamp.
                         dumpFilesMap[dbName] = append(dumpFilesMap[dbName], file)
                 }
         }
 
+	// compare with the number specified in the config.
         for _, dbDumpFiles := range dumpFilesMap {
                 if len(dbDumpFiles) <= config.DumpGenerations {
                         continue
                 }
 
-                // ファイルを更新日時でソート
                 sortByModTime(dbDumpFiles)
 
-                // 古いファイルを削除し、指定した世代数だけ最新のファイルを保持
+                // delete old file. 
                 for i, f := range dbDumpFiles {
                         if i < len(dbDumpFiles)-config.DumpGenerations {
                                 err = os.Remove(filepath.Join(config.DumpDir, f.Name()))
@@ -194,23 +249,27 @@ func cleanupOldDumps(config *Config) error {
         return nil
 }
 
-func sortByModTime(files []os.FileInfo) {
-	sort.Slice(files, func(i, j int) bool {
-		return files[i].ModTime().Before(files[j].ModTime())
-	})
-}
-
 func main() {
+	// Setup logfile
+	logFile, err := setupLogger()
+	if err != nil {
+		log.Fatalf("%s systemd[1]: %s", time.Now().Format("Jan 02 15:04:05"), err)
+	}
+	defer logFile.Close()
+
+	// Read config
 	config, err := readConfig()
 	checkError(err)
 
+	// MySQL Active check
 	err = runMySQLActiveCheck(config)
 	checkError(err)
 
+	// Running mysqldump
 	err = runMySQLDump(config)
 	checkError(err)
 
+	// Generation management of dump files
 	err = cleanupOldDumps(config)
 	checkError(err)
 }
-
