67341779bc34060796b6a125d39adfe1e29faaeb
t0ge-kun <kasipan12345678@gmail.com>
2023-07-26 14:56:15 +0900

    fix: refactoring cleanup old dump process.

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 	"time"
 
 	"mysqldump/pkg/logging"
@@ -24,56 +19,9 @@ func checkError(err error) {
 	}
 }
 
-// Subfunc cleanupOldDumps: Time sort dump files 
-func sortByModTime(files []os.FileInfo) {
-        sort.Slice(files, func(i, j int) bool {
-                return files[i].ModTime().Before(files[j].ModTime())
-        })
-}
-
-// Mainfunc: Generation management of dump files
-func cleanupOldDumps(config *readconfig.Config) error {
-        files, err := ioutil.ReadDir(config.DumpDir)
-        if err != nil {
-                return err
-        }
-
-        dumpFilesMap := make(map[string][]os.FileInfo)
-
-        for _, file := range files {
-                if strings.HasPrefix(file.Name(), "dump_") && strings.HasSuffix(file.Name(), ".sql") {
-                        dbName := strings.TrimSuffix(strings.TrimPrefix(file.Name(), "dump_"), ".sql")
-                        dbName = strings.Split(dbName, "_")[0] // Remove the part after timestamp.
-                        dumpFilesMap[dbName] = append(dumpFilesMap[dbName], file)
-                }
-        }
-
-	// compare with the number specified in the config.
-        for _, dbDumpFiles := range dumpFilesMap {
-                if len(dbDumpFiles) <= config.DumpGenerations {
-                        continue
-                }
-
-                sortByModTime(dbDumpFiles)
-
-                // delete old file. 
-                for i, f := range dbDumpFiles {
-                        if i < len(dbDumpFiles)-config.DumpGenerations {
-                                err = os.Remove(filepath.Join(config.DumpDir, f.Name()))
-                                if err != nil {
-                                        return err
-                                }
-                        }
-                }
-        }
-
-        return nil
-}
-
 func main() {
 	// Setup logfile
 	logFile, err := logging.SetupLogger()
-
 	if err != nil {
 		log.Fatalf("%s systemd[1]: %s", time.Now().Format("Jan 02 15:04:05"), err)
 	}
@@ -92,6 +40,6 @@ func main() {
 	checkError(err)
 
 	// Generation management of dump files
-	err = cleanupOldDumps(config)
+	err = mysqldump.CleanupOldDumps(config)
 	checkError(err)
 }
---
+++ pkg/mysqldump/cleanupolddump.go
@@ -0,0 +1,58 @@
+package mysqldump
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"mysqldump/pkg/readconfig"
+)
+
+// Subfunc sortByModTime: Time sort dump files
+func sortByModTime(files []os.FileInfo) {
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].ModTime().Before(files[j].ModTime())
+	})
+}
+
+// Mainfunc CleanupOldDumps: Generation management of dump files
+func CleanupOldDumps(config *readconfig.Config) error {
+	files, err := ioutil.ReadDir(config.DumpDir)
+	if err != nil {
+		return err
+	}
+
+	dumpFilesMap := make(map[string][]os.FileInfo)
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), "dump_") && strings.HasSuffix(file.Name(), ".sql") {
+			dbName := strings.TrimSuffix(strings.TrimPrefix(file.Name(), "dump_"), ".sql")
+			dbName = strings.Split(dbName, "_")[0] // Remove the part after timestamp.
+			dumpFilesMap[dbName] = append(dumpFilesMap[dbName], file)
+		}
+	}
+
+	// compare with the number specified in the config.
+	for _, dbDumpFiles := range dumpFilesMap {
+		if len(dbDumpFiles) <= config.DumpGenerations {
+			continue
+		}
+
+		sortByModTime(dbDumpFiles)
+
+		// delete old file.
+		for i, f := range dbDumpFiles {
+			if i < len(dbDumpFiles)-config.DumpGenerations {
+				err = os.Remove(filepath.Join(config.DumpDir, f.Name()))
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
